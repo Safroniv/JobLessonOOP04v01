ec62a067ac4be5a9ed0f391a484d84f415f1d6c3
safroniv <safroniv@mail.ru>
2022-04-16 21:19:43 +0300

    ООП Урок 4 Задание 1 - готово.

--- JobLessonOOP04v01Part01/Program.cs
+++ JobLessonOOP04v01Part01/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JobLessonOOP04v01Part01
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //Здание #1
+            Buildings newBuilding1 = new Buildings();
+            newBuilding1.SetBuildingNumber();
+            newBuilding1.SetHeightBuilding(48.3);
+            newBuilding1.SetFloorsBuilding(18);
+            newBuilding1.SetNumberOfApartamentsBuilding(450);
+            newBuilding1.SetNumberOfEntrancesBuilding(5);
+            newBuilding1.PrintInfo();
+
+            //Здание #2
+            Buildings newBuilding2 = new Buildings();
+            newBuilding2.SetBuildingNumber();
+            newBuilding2.SetHeightBuilding(50.5);
+            newBuilding2.SetFloorsBuilding(20);
+            newBuilding2.SetNumberOfApartamentsBuilding(400);
+            newBuilding2.SetNumberOfEntrancesBuilding(5);
+            newBuilding2.PrintInfo();
+
+            Console.ReadLine();
 
         }
     }
